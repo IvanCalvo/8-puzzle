3ec0fad13574e9e35f05a93d5c5c3359298bd722
Ivan Calvo <ivandc1998.ic@gmail.com>
2018-06-18 15:05:46 -0300

    adicionando funcao troca

--- projeto_versao_1.c
+++ projeto_versao_1.c
@@ -92,6 +92,33 @@ int compara(int** a, int** b){
 	return x;
 }
 
+int** troca(int num1, int num2, int** jogo){
+	int aux;
+	int i, j, x, y;
+	int pos_i, pos_j;
+	for(i=0; i<3; i++){
+		for(j=0; j<3; j++){
+			if(jogo[i][j]==num1){
+				aux = num1;
+				jogo[i][j] = num2;
+				pos_i=i;
+				pos_j=j;
+				break;
+			}
+		}
+	}
+	for(x=0; x<3; x++){
+		for(y=0; y<3; y++){
+			if(jogo[x][y]==num2){
+				if(x!=pos_i || y!=pos_j){
+					jogo[x][y] = aux;
+				}
+			}
+		}
+	}
+	return jogo;
+}
+
 int main(){
 	srand(time(NULL));
 	int** jogo = aloca_matriz(3, 3);
@@ -101,6 +128,9 @@ int main(){
 	int** resposta = aloca_matriz(3,3);
 	resposta = cria_resposta(resposta);
 	printf("\nResposta\n");
+	imprime_matriz(resposta);
+	printf("\nTroca 5 e 8\n");
+	resposta = troca(8, 5, resposta);
 	imprime_matriz(resposta);
 	return 0;
 }
